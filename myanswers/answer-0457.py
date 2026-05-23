d98a77175ae203e1e03610883816728a7fd95883
BayGT <bayron.galeanot@udea.edu.co>
2026-05-23 18:49:26 -0500

    Update answer-0457.py

--- myanswers/answer-0457.py
+++ myanswers/answer-0457.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def resumen_clientes(df):
-    resumen = (
+    return (
         df.groupby("cliente", as_index=False)
           .agg(
               total_gastado=("monto", "sum"),
@@ -11,5 +11,3 @@ def resumen_clientes(df):
           .sort_values("total_gastado", ascending=False)
           .reset_index(drop=True)
     )
-
-    return resumen
