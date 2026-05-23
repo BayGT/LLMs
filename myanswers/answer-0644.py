f31725bcccbb6c4fc5aef0e56a3c702e5f1e2ec3
BayGT <bayron.galeanot@udea.edu.co>
2026-05-23 18:43:16 -0500

    Update answer-0644.py

--- myanswers/answer-0644.py
+++ myanswers/answer-0644.py
@@ -2,27 +2,18 @@ import pandas as pd
 from sklearn.impute import KNNImputer
 
 def imputar_con_knn(df, n_neighbors, target_col):
-    """
-    Imputa los valores faltantes de las columnas numéricas usando KNNImputer,
-    dejando la columna objetivo sin modificar y al final del DataFrame.
-    """
-
-    # Separar variables predictoras y columna objetivo
     features_df = df.drop(columns=[target_col])
     target = df[target_col]
 
-    # Aplicar KNNImputer a las variables predictoras
     imputer = KNNImputer(n_neighbors=n_neighbors)
     X_imputed = imputer.fit_transform(features_df)
 
-    # Reconstruir el DataFrame con el mismo índice y nombres de columnas
     df_resultado = pd.DataFrame(
         X_imputed,
         columns=features_df.columns,
         index=df.index
     )
 
-    # Agregar la columna objetivo al final
     df_resultado[target_col] = target.values
 
     return df_resultado
